816ad99b55c781bcd3e41140a94708c3665d17ac
Nguyen <hai.b.nguyen@accenturefederal.com>
2019-09-20 10:08:29 -0500

    binding form model

--- src/app/profile/user.module.ts
+++ src/app/profile/user.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { usersRoutes } from './user.routes';
 import { ProfileComponent } from './profile.component';
 import { LoginComponent } from '../login/login.component';
@@ -8,6 +9,7 @@ import { LoginComponent } from '../login/login.component';
 @NgModule({
     imports: [
         CommonModule,
+        FormsModule,
         RouterModule.forChild(usersRoutes)
     ],
     declarations: [ProfileComponent, LoginComponent],
