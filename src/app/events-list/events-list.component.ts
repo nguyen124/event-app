4fac750b02feb5535e6bc81d95dc2f60eb082fe9
Nguyen <hai.b.nguyen@accenturefederal.com>
2019-09-20 09:50:38 -0500

    add model interface

--- src/app/events-list/events-list.component.ts
+++ src/app/events-list/events-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { EventService } from '../event-services.service';
 import { ToastrService } from '../shared/toastr.service';
 import { ActivatedRoute } from '@angular/router';
+import { IEvent } from '../shared/event.model';
 
 @Component({
   selector: 'app-events-list',
@@ -9,7 +10,7 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./events-list.component.css']
 })
 export class EventsListComponent implements OnInit {
-  events
+  events: IEvent[];
   constructor(private eventSvc: EventService, private toastr: ToastrService, private route: ActivatedRoute) { }
 
   handleClick(eventName) {
