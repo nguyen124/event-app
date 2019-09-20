d3c90855c99017a5fc794da8293ea9e7c34c7a15
Nguyen <hai.b.nguyen@accenturefederal.com>
2019-09-20 15:23:42 -0500

    create session form validator

--- src/app/create-session/create-session.component.ts
+++ src/app/create-session/create-session.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { ISession } from '../shared/event.model';
 
 @Component({
   selector: 'app-create-session',
@@ -6,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-session.component.css']
 })
 export class CreateSessionComponent implements OnInit {
-
+  name: FormControl;
+  presenter: FormControl;
+  duration: FormControl;
+  level: FormControl;
+  abstract: FormControl;
+  newSessionForm: FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.name = new FormControl('', Validators.required);
+    this.presenter = new FormControl('', Validators.required);
+    this.duration = new FormControl('', Validators.required);
+    this.level = new FormControl('', Validators.required);
+    this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400)]);
+
+    this.newSessionForm = new FormGroup({
+      name: this.name,
+      presenter: this.presenter,
+      duration: this.duration,
+      level: this.level,
+      abstract: this.abstract
+    });
+
   }
 
+  saveSession(formValues) {
+    let session: ISession = {
+      id: undefined,
+      voters: [],
+      name: formValues.name,
+      duration: +formValues.duration,
+      level: formValues.level,
+      presenter: formValues.presenter,
+      abstract: formValues.abstract
+    }
+  }
 }
