8f7f4965c04d17e5de859c91b2c9b8ee1bae499d
Nguyen <hai.b.nguyen@accenturefederal.com>
2019-09-23 13:34:58 -0500

    modal trigger binding id

--- src/app/common/trigger-modal.directive.ts
+++ src/app/common/trigger-modal.directive.ts
@@ -6,13 +6,14 @@ import { JQ_TOKEN } from '../shared/jQuery.service';
 })
 export class TriggerModalDirective implements OnInit {
   private el: HTMLHtmlElement;
+  @Input("modal-trigger") modalId: string;
   constructor(ref: ElementRef, @Inject(JQ_TOKEN) private $: any) {
     this.el = ref.nativeElement;
   }
 
   ngOnInit() {
     this.el.addEventListener('click', e => {
-      this.$('#simple-modal').modal({});
+      this.$(`#${this.modalId}`).modal({});
     })
 
   }
