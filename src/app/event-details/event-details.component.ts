754b6b2df14bff458a6ce16c745410dc75bf54b2
Nguyen <hai.b.nguyen@accenturefederal.com>
2019-09-23 16:04:53 -0500

    routing to the same component

--- src/app/event-details/event-details.component.ts
+++ src/app/event-details/event-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from '../event-services.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { IEvent, ISession } from '../shared/event.model';
 
 @Component({
@@ -12,18 +12,24 @@ export class EventDetailsComponent implements OnInit {
   event: IEvent;
   addMode: boolean;
   filterBy: string = 'all';
-  sortBy: string ='name'
+  sortBy: string = 'name'
 
-  constructor(private eventSvc: EventService, private activatedRoute: ActivatedRoute) { }
+  constructor(private eventSvc: EventService, private activatedRoute: ActivatedRoute, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.forEach((params: Params) => {
+      this.event = this.eventSvc.getEvent(+params['id']);
+      this.addMode = false;
+    })
     this.event = this.eventSvc.getEvent(this.activatedRoute.snapshot.params['id']);
   }
 
   addSession() {
     this.addMode = true;
   }
-
+  cancelAddSession() {
+    this.addMode = false;
+  }
   saveNewSession(session: ISession) {
     const nextId = Math.max.apply(null, this.event.sessions.map(s => s.id));
     session.id = nextId + 1;
