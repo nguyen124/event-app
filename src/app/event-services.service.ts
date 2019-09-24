55a2474f01f7c92d13485e8ce41d1b0403ae66f1
Nguyen <hai.b.nguyen@accenturefederal.com>
2019-09-24 09:08:47 -0500

    move data to server

--- src/app/event-services.service.ts
+++ src/app/event-services.service.ts
@@ -34,22 +34,9 @@ export class EventService {
       .pipe(catchError(this.handleError<IEvent>('saveEvents')));
   }
 
-  searchSessions(term: any) {
-    var term = term.toLocaleLowerCase();
-    var results: ISession[] = [];
-    EVENTS.forEach(event => {
-      var matchingSessions = event.sessions.filter(session => session.name.toLocaleLowerCase().indexOf(term) > -1);
-      matchingSessions = matchingSessions.map((session: any) => {
-        session.eventId = event.id;
-        return session;
-      })
-      results = results.concat(matchingSessions);
-    });
-    var emitter = new EventEmitter(true);
-    setTimeout(() => {
-      emitter.emit(results);
-    }, 100);
-    return emitter;
+  searchSessions(term: any): Observable<ISession[]> {
+    return this.http.get<ISession[]>("/api/sessions/search?search=" + term)
+      .pipe(catchError(this.handleError<ISession[]>('searchSessions')));
   }
 
 }
